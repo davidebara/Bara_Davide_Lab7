472f453232203efafeda5cdd7789c7769d3e4ac6
Davide <dmrbara@gmail.com>
2023-11-27 18:10:32 +0200

    Laborator8 complet

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace Bara_Davide_Lab7
+﻿using System;
+using Bara_Davide_Lab7.Data;
+using System.IO;
+
+
+namespace Bara_Davide_Lab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
